8f1ed75df84fae3a920a93c6cefbe22cc9b9d2ba
Yellow <yellowaformac@gmail.com>
2018-05-22 18:40:27 +0200

    push assets with first try

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 
+
+app.use(express.static(__dirname + '/assets'));
+
 app.get('/', function(req, res){
-    res.status(200).send("Salut ceci est la page d'acceuil");
+    res.status(200).sendFile(__dirname + '/index.html');
 });
 
 app.get('*', function(req, res){
-    res.status(404).send("Erreur 404");
+    res.status(404).sendFile(__dirname + '/404.html');
 });
 
 app.listen(8000);
