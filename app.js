49978b09fd0d849f6e7ff72c51b22db3adfad640
Yellow <yellowaformac@gmail.com>
2018-05-29 11:15:54 +0200

    push redirect admin

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const express = require('express'); // On utilise le framework Express pour Nodejs
 const path = require('path'); // Module fourni avec express pour les chemins. (Pour utiliser "join")
 const bodyParser = require('body-parser'); // Permet d'utiliser la method post data
-const expressValidator = require('express-validator'); // Permet de required les inputs
-const mongodb = require('mongodb');
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/visualdnb');
-let db = mongoose.connection;
-
-db.once('open', function(){
-    console.log('connected to mongodb');
-})
 
 // Routes
 const indexRoutes = require('./routes/index.js'); // "importe" les routes de l'index.hbs
 const errorRoutes = require('./routes/404.js'); // idem qu'au dessus pour la page 404
-const adminRoutes = require('./routes/admin.js');
-const membersRoutes = require('./routes/members.js');
 
 // Init app Express
 const app = express(); 
@@ -34,8 +22,6 @@ app.use(express.static(__dirname + '/assets')); // On dit que assets contient le
 // __dirname permet de récupérer l'url d'où on est à partir de la racine
 
 // Declaration de nos routes maintenant à leurs place dans le dossier routes
-app.use('/', adminRoutes);
-app.use('/', membersRoutes);
 app.use('/', indexRoutes);
 app.use('*', errorRoutes);
 
