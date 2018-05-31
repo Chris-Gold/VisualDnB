db330dea64d412ad323ea18ae6c03c57a9f06ea1
Chris Gold <chris.ratsimba87@gmail.com>
2018-05-31 09:37:26 +0200

    chris front

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,19 @@
         $('#da-slider').cslider({
 					bgincrement	: 0
 				});
+        // INSTAFEED
+        var userFeed = new Instafeed({
+          get: 'user',
+          userId : '6921109790',
+          resolution : 'standard_resolution',
+          accessToken : '6921109790.1677ed0.7ad1c946a4b74af5bf14e5575f1052e8',
+          sortBy : 'most-recent',
+          //template : '<li><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></li>'
+          template : '<li><img src="{{image}}" alt="{{caption}}" width="290" height="200"/></li>'
+
+          // after : function () {$('#instafeed > div:nth-child(1)').addClass('active');}
+        });
+        userFeed.run();
+        
   });
 })(jQuery);
