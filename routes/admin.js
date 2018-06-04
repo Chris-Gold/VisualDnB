1800931cbc59910534bbf2dd610ebc4c7c1972cc
Yellow <yellowaformac@gmail.com>
2018-06-04 15:22:53 +0200

    pull taf Chris

--- routes/admin.js
+++ routes/admin.js
@@ -6,19 +6,19 @@ router.get('/', function(req, res){
     res.status(200).render('back/index.hbs');
 });
 
-router.post('/', function(req, res){
-    var bp = new BlogPost();
-    bp.title = req.body.title;
-    bp.message = req.body.message;
-    bp.save(function(err){
-        if (err){
-            res.render('back/index.hbs', {title: "Administration", description: "Une erreur s'est produite"});
-        }
-        res.render('back/index.hbs', {title: "Administration", description: "Blog post sauvegardé"});
-    })
-    function redirect() {
-        window.location.assign("http://localhost:8000/admin#presta");
-    }
-});
+// router.post('/', function(req, res){
+//     var bp = new BlogPost();
+//     bp.title = req.body.title;
+//     bp.message = req.body.message;
+//     bp.save(function(err){
+//         if (err){
+//             res.render('back/index.hbs', {title: "Administration", description: "Une erreur s'est produite"});
+//         }
+//         res.render('back/index.hbs', {title: "Administration", description: "Blog post sauvegardé"});
+//     })
+//     function redirect() {
+//         window.location.assign("http://localhost:8000/admin#presta");
+//     }
+// });
 
 module.exports = router;
