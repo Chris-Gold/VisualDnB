0dc2419bb31cda5f854963832e6751c446a23c79
Chris Gold <chris.ratsimba87@gmail.com>
2018-06-29 15:57:02 +0200

    fridaaaaaaaaay

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     rename = require('gulp-rename'),
-    autoprefixer = require('autoprefixer');
+    autoprefixer = require('autoprefixer'),
+    nano = require('cssnano');
 
-gulp.task('css', function(){
+var processor = [
+  autoprefixer({ browsers: ['last 5 versions'] }),
+  nano
+]
+
+gulp.task('default', function(){
   gulp.src('assets/css/style.css')
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-    .pipe(rename('bundle.css'))
+    .pipe(postcss(processor))
+    .pipe(rename('bundle.min.css'))
     .pipe(gulp.dest('assets/css/'))
 })
